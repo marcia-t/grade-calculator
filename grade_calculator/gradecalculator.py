c892542301a0227f257eda854d9e7020fb6abc3b
Marcia Tejeda <tejedamarcia@gmail.com>
2022-06-30 12:50:26 +0200

    Logging

--- grade_calculator/gradecalculator.py
+++ grade_calculator/gradecalculator.py
@@ -1,21 +1,24 @@
 import boto3
 import json
 import os
+import logging
 
 
 s3 = boto3.client('s3')
 sns_client = boto3.client('sns')
+logger = logging.getLogger('gradecalculator')
+logger.setLevel(logging.INFO)
 
 
 def lambda_handler(event, context):
     topic = os.environ.get('GRADE_CALCULATOR_TOPIC')
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     file_key = event['Records'][0]['s3']['object']['key']
+    logger.info('Reading {} from {}'.format(file_key, bucket_name))
     obj = s3.get_object(Bucket=bucket_name, Key=file_key)
     file_content = obj['Body'].read().decode('utf-8')
     checkout_events = json.loads(file_content)
     for each_event in checkout_events:
-        print(each_event)
         score = each_event['testScore']
         if score > 70:
             grade = 'A'
@@ -23,6 +26,8 @@ def lambda_handler(event, context):
             grade = 'B'
         else: grade = 'C'
         each_event['grade'] = grade
+        logger.info('Message being published')
+        logger.info(each_event)
         sns_client.publish(
             TopicArn=topic,
             Message=json.dumps({'default': json.dumps(each_event)}),
--- grade_calculator/reportgenerator.py
+++ grade_calculator/reportgenerator.py
@@ -1,3 +1,10 @@
+import logging
+
+
+logger = logging.getLogger('gradecalculator')
+logger.setLevel(logging.INFO)
+
+
 def lambda_handler(event, context):
     message = event['Records'][0]['Sns']['Message']
-    print(message)
+    logger.info(message)
